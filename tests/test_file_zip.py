eb479bc6377ae05479726ae2da27db76183459d7
Owen Chia <aptx945@gmail.com>
2019-03-13 15:25:21 +0800

    fix test

--- tests/test_file_zip.py
+++ tests/test_file_zip.py
@@ -8,8 +8,10 @@ from core import messages
 import subprocess
 import os
 
+
 def setUpModule():
-    subprocess.check_output("""
+    subprocess.check_output(
+        """
 BASE_FOLDER="{config.base_folder}/test_file_zip/"
 rm -rf "$BASE_FOLDER"
 
@@ -28,9 +30,9 @@ rm -rf "$BASE_FOLDER/dir1"
 
 chown www-data: -R "$BASE_FOLDER/"
 
-""".format(
-config = config
-), shell=True)
+""".format(config=config),
+        shell=True)
+
 
 class FileZip(BaseTest):
 
@@ -40,39 +42,24 @@ class FileZip(BaseTest):
         'test_file_zip/dir1/dir2/dir3',
         'test_file_zip/dir1/dir2/dir3/dir4',
     ]
-    folders_abs = [ 
-        os.path.join(config.base_folder, f) 
-        for f in folders_rel 
-    ]
-    
+    folders_abs = [os.path.join(config.base_folder, f) for f in folders_rel]
+
     files_rel = [
         'test_file_zip/dir1/f1',
         'test_file_zip/dir1/dir2/f2',
         'test_file_zip/dir1/dir2/dir3/f3',
         'test_file_zip/dir1/dir2/dir3/dir4/f4',
     ]
-    files_abs = [ 
-        os.path.join(config.base_folder, f) 
-        for f in files_rel 
-    ]
-        
-    zips_rel = [
-        'test_file_zip/test_0.zip'
-    ]
-    zips_abs = [ 
-        os.path.join(config.base_folder, f) 
-        for f in zips_rel 
-    ]
-    
+    files_abs = [os.path.join(config.base_folder, f) for f in files_rel]
+
+    zips_rel = ['test_file_zip/test_0.zip']
+    zips_abs = [os.path.join(config.base_folder, f) for f in zips_rel]
+
     other_file_rel = 'test_file_zip/f5'
-    other_file_abs = os.path.join(config.base_folder, other_file_rel) 
-    
+    other_file_abs = os.path.join(config.base_folder, other_file_rel)
+
     def setUp(self):
-        self.session = SessionURL(
-                    self.url,
-                    self.password,
-                    volatile = True
-                    )
+        self.session = SessionURL(self.url, self.password, volatile=True)
 
         modules.load_modules(self.session)
 
@@ -81,46 +68,67 @@ class FileZip(BaseTest):
     def test_compress_decompress(self):
 
         # Uncompress test.zip
-        self.assertTrue(self.run_argv(["--decompress", self.zips_rel[0], 'test_file_zip/' ]));
+        self.assertTrue(
+            self.run_argv(["--decompress", self.zips_rel[0],
+                           'test_file_zip/']))
         for file in self.files_abs:
-            self.assertEqual(subprocess.check_output("cat %s" % file, shell=True),'1')
+            self.assertEqual(
+                subprocess.check_output("cat %s" % file, shell=True), b'1')
         for folder in self.folders_abs:
             subprocess.check_call('stat -c %%a "%s"' % folder, shell=True)
 
         # Compress it again giving szipting folder
-        self.assertTrue(self.run_argv(['test_file_zip/test_1.zip', self.folders_rel[0]]));
+        self.assertTrue(
+            self.run_argv(['test_file_zip/test_1.zip', self.folders_rel[0]]))
         self.zips_rel.append('test_file_zip/test_1.zip')
-        self.zips_abs.append(os.path.join(config.base_folder, self.zips_rel[-1]))
+        self.zips_abs.append(
+            os.path.join(config.base_folder, self.zips_rel[-1]))
 
         # Uncompress the new archive and recheck
-        self.assertTrue(self.run_argv(["--decompress", 'test_file_zip/test_1.zip', 'test_file_zip/']));
+        self.assertTrue(
+            self.run_argv(
+                ["--decompress", 'test_file_zip/test_1.zip',
+                 'test_file_zip/']))
         for file in self.files_abs:
-            self.assertEqual(subprocess.check_output("cat %s" % file, shell=True),'1')
+            self.assertEqual(
+                subprocess.check_output("cat %s" % file, shell=True), b'1')
         for folder in self.folders_abs:
             subprocess.check_call('stat -c %%a "%s"' % folder, shell=True)
 
     def test_compress_multiple(self):
-    
+
         # Uncompress test.zip
-        self.assertTrue(self.run_argv(["--decompress", self.zips_rel[0], 'test_file_zip/' ]));
+        self.assertTrue(
+            self.run_argv(["--decompress", self.zips_rel[0],
+                           'test_file_zip/']))
         for file in self.files_abs:
-            self.assertEqual(subprocess.check_output("cat %s" % file, shell=True),'1')
+            self.assertEqual(
+                subprocess.check_output("cat %s" % file, shell=True), b'1')
         for folder in self.folders_abs:
             subprocess.check_call('stat -c %%a "%s"' % folder, shell=True)
-    
+
         # Create a new zip adding also other_file
-        self.assertTrue(self.run_argv(['test_file_zip/test_2.zip', self.folders_rel[0], self.other_file_rel]));
+        self.assertTrue(
+            self.run_argv([
+                'test_file_zip/test_2.zip', self.folders_rel[0],
+                self.other_file_rel
+            ]))
         self.zips_rel.append('test_file_zip/test_2.zip')
-        self.zips_abs.append(os.path.join(config.base_folder, self.zips_rel[-1]))
-    
+        self.zips_abs.append(
+            os.path.join(config.base_folder, self.zips_rel[-1]))
+
         # Remove all the files
         subprocess.check_output("rm -rf %s" % self.folders_abs[0], shell=True)
         subprocess.check_output("rm %s" % self.other_file_abs, shell=True)
 
         # Uncompress the new archive and recheck
-        self.assertTrue(self.run_argv(["--decompress", 'test_file_zip/test_2.zip', 'test_file_zip/']));
+        self.assertTrue(
+            self.run_argv(
+                ["--decompress", 'test_file_zip/test_2.zip',
+                 'test_file_zip/']))
         for file in self.files_abs:
-            self.assertEqual(subprocess.check_output("cat %s" % file, shell=True),'1')
+            self.assertEqual(
+                subprocess.check_output("cat %s" % file, shell=True), b'1')
         for folder in self.folders_abs:
             subprocess.check_call('stat -c %%a "%s"' % folder, shell=True)
 
@@ -139,28 +147,29 @@ class FileZip(BaseTest):
 
         #self.assertEqual(subprocess.check_output("cat %s" % self.other_file_abs, shell=True),'1')
 
-
     @log_capture()
     def test_already_exists(self, log_captured):
-    
-            # Create a new zip with other_file, with the name test_0.zip
-        self.assertIsNone(self.run_argv(['test_file_zip/test_0.zip', self.other_file_rel]));
-        self.assertEqual(log_captured.records[-1].msg,
-                         "File 'test_file_zip/test_0.zip' already exists, skipping compressing")
-    
+
+        # Create a new zip with other_file, with the name test_0.zip
+        self.assertIsNone(
+            self.run_argv(['test_file_zip/test_0.zip', self.other_file_rel]))
+        self.assertEqual(
+            log_captured.records[-1].msg,
+            "File 'test_file_zip/test_0.zip' already exists, skipping compressing"
+        )
 
     @log_capture()
     def test_unexistant_decompress(self, log_captured):
-    
-        self.assertIsNone(self.run_argv(["--decompress", 'bogus', '.']));
-        self.assertEqual(log_captured.records[-1].msg,
-                         "Skipping file 'bogus', check existance and permission")
-    
-    
+
+        self.assertIsNone(self.run_argv(["--decompress", 'bogus', '.']))
+        self.assertEqual(
+            log_captured.records[-1].msg,
+            "Skipping file 'bogus', check existance and permission")
+
     @log_capture()
     def test_unexistant_compress(self, log_captured):
-    
-        self.assertIsNone(self.run_argv(['bogus.zip', 'bogus']));
-        self.assertEqual(log_captured.records[-1].msg,
-                         "File 'bogus.zip' not created, check existance and permission")
-    
+
+        self.assertIsNone(self.run_argv(['bogus.zip', 'bogus']))
+        self.assertEqual(
+            log_captured.records[-1].msg,
+            "File 'bogus.zip' not created, check existance and permission")
